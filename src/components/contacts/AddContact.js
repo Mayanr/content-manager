0180780c6be525fa29706e31393fc64ec44c0eed
Mayan Rothstein <mayan.rothstein@umusic.com>
2022-03-13 23:14:17 -0700

    basic frontend structure completed

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 let AddContact = () => {
     return (
         <React.Fragment>
-            <h2>AddContact</h2>
+            <section className="add-contact p-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p className="h3 text-success fw-bold">Create Contact</p>
+                            <p className="fst-italic">placeholder text</p>
+                        </div>
+                        <div className="row">
+                            <dv className="co-md-4">
+                                <form>
+                                    <div className="mb-2">
+                                        <input type="text" className="form-control" placeholder="name"/>
+                                        <input type="text" className="form-control" placeholder="photo URL"/>
+                                        <input type="number" className="form-control" placeholder="mobile"/>
+                                        <input type="text" className="form-control" placeholder="email"/>
+                                        <input type="text" className="form-control" placeholder="company"/>
+                                        <input type="text" className="form-control" placeholder="title"/>
+                                        <select className="form-control">
+                                            <option value=""> Select a Group</option>
+                                        </select>
+                                    </div>
+                                    <div className="mb-2">
+                                        <input type="submit" className="btn btn-success" value="Create"/>
+                                        <Link to={'/contacts/list'} className="btn btn-dark ms-2">Cancel</Link>
+                                    </div>
+                                </form>
+                            </dv>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     );
 };
--- src/components/contacts/ViewContact.js
+++ src/components/contacts/ViewContact.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 let ViewContact = () => {
     return (
         <React.Fragment>
-            <h2>ViewContact</h2>
+            <section className="view-contact p-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p className="h3 text-warning">View Contact</p>
+                            <p className="fst italic">Placeholder</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="view-contact mt-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <img src="https://www.vhv.rs/dpng/d/544-5445462_people-icons-png-flat-person-icon-png-transparent.png" alt="phot goes here" className="img-fluid contact-img" />                            
+                        </div>
+                        <div className="col-md-8">
+                        <ul className="list-group">
+                            <li className="list-group-item list-group-item-action">
+                                Name: Kelly
+                            </li>
+                            <li className="list-group-item list-group-item-action">
+                                Mobile: 879-878-1209
+                            </li>
+                            <li className="list-group-item list-group-item-action">
+                                Email: kellyT@gmail.com
+                            </li>
+                            <li className="list-group-item list-group-item-action">
+                                Company: UMG
+                            </li>
+                            <li className="list-group-item list-group-item-action">
+                                Title: Manager
+                            </li>
+                            <li className="list-group-item list-group-item-action">
+                                Group: Marketing
+                            </li>
+                        </ul>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <Link to={'/contacts/list'} className="btn btn-warning">Back</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     );
 };
